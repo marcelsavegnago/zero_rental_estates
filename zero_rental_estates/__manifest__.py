71c5db4094ba0893c7b3b06103d42d8595356369
ZeroSystems <sales@erpzero.com>
2019-08-31 06:35:37 +0200

    Update __manifest__.py

--- zero_rental_estates/__manifest__.py
+++ zero_rental_estates/__manifest__.py
@@ -1,22 +1,26 @@
+# Copyright 2019 Zero for information systems(https://www.erpzero.com).
 
-# -*- coding: utf-8 -*-
 {
-    'name': "rental_estate_management",
-
-    'summary': 'rental property management',
-
-    'description': """rental property management""",
-
-    'author': "zero systems",
-    'website': "http:/www.erpzero.com",
+    'name': 'rental_estate_management',
+    'version': '11.0.1',
+    'summary': """rental property management""",
+    'description': """This module helps you to Manage rental property""",
     'category': 'real estate',
-    'version': '11.0.0.1',
+    'author': 'Zero Systems',
+    'company': 'Zero for Information Systems',
+    'website': "https://www.erpzero.com",
+    'email': "sales@erpzero.com",
+    'depends': ['base','mail','portal','resource','analytic'],
+    'data': [
+        'security/security.xml','security/ir.model.access.csv',
+        'views/city_districts_view.xml','views/lessor_view.xml',
+        'views/partner_view.xml','views/payment_report.xml',
+        'views/rental_view.xml','views/estate_status_view.xml',
+        'views/estate_view.xml,
+    ],
+    'images': ['static/description/logo.PNG'],
+    'demo': [],
     'installable': True,
     'auto_install': False,
     'application': True,
-     # any module necessary for this one to work correctly
-    'depends': ['base','mail','portal','resource','analytic'],
-    'images': ['static/description/logo.PNG'],
-    # always loaded
-    'data': ['security/security.xml','security/ir.model.access.csv','views/city_districts_view.xml','views/lessor_view.xml','views/partner_view.xml','views/payment_report.xml','views/potential_partner_view.xml','views/rental_view.xml','views/estate_status_view.xml','views/estate_view.xml',],
 }
