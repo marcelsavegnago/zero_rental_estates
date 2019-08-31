9bcda15ea5fc0804a3aa6c377987397854f4cf96
ZeroSystems <sales@erpzero.com>
2019-08-31 16:53:13 +0200

    Update __init__.py

--- zero_rental_estates/__init__.py
+++ zero_rental_estates/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import biens
-from . import locataires
-from . import locations
-from . import etat_des_lieux
-from . import bailleur
-from . import locataire_potentiel
-from . import villes_quartiers
+from . import controllers
+from . import models
