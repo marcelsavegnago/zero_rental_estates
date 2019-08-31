ae25927d29a63ff505fb82955492650879cc236d
ZeroSystems <sales@erpzero.com>
2019-08-31 12:02:52 +0200

    Update and rename city_districts.py to city_district.py

--- zero_rental_estates/models/city_district.py
+++ zero_rental_estates/models/city_district.py
@@ -5,17 +5,19 @@ from odoo.tools import float_compare, pycompat
 from odoo.exceptions import ValidationError
 from odoo.osv import expression
 
-class city(models.Model):
+
+
+class City(models.Model):
     _name = 'lb.city'
     _rec_name = 'name'
 
-    name = fields.Char(string="City")
+    nom = fields.Char(string="City")
 
 
-class district(models.Model):
+class District(models.Model):
     _name = 'lb.district'
-    _rec_name = 'district_name'
+    _rec_name = 'name_district'
 
-    district_name = fields.Char(string="District")
+    name_district = fields.Char(string="District")
     city_partner = fields.Many2one('lb.city')
 
