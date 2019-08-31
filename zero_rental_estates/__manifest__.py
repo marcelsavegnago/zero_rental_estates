c7a4f025bfecf014ad772c1d307e11b381983e6d
ZeroSystems <sales@erpzero.com>
2019-08-31 06:09:15 +0200

    Update __manifest__.py

--- zero_rental_estates/__manifest__.py
+++ zero_rental_estates/__manifest__.py
@@ -16,7 +16,7 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base','mail','portal','resource','analytic'],
-    'images': ['static/description/icon.png'],
+    'images': ['static/description/logo.PNG'],
     # always loaded
     'data': ['security/security.xml','security/ir.model.access.csv','views/city_districts_view.xml','views/lessor_view.xml','views/partner_view.xml','views/payment_report.xml','views/potential_partner_view.xml','views/rental_view.xml','views/estate_status_view.xml','views/estate_view.xml',],
 }
