2abefc4763c38b76e5b4da705b9e12e890e4092e
ZeroSystems <sales@erpzero.com>
2019-09-05 00:57:49 +0200

    Update and rename partner.py to tenants.py

--- zero_rental_estates/models/tenants.py
+++ zero_rental_estates/models/tenants.py
@@ -6,23 +6,23 @@ from odoo.exceptions import ValidationError
 from odoo.osv import expression
 
 from odoo.addons import decimal_precision as dp
-    
 
-class Partner(models.Model):
+
+class Tenant(models.Model):
     _inherit = 'res.partner'
 
-    identity_type = fields.Selection([('NCI','National card d\'identity'),('Visit_Card','Visa Cart'),('passport','Passport')],string='Type of identity d\'identity')
-    identity_id = fields.Char(string='Number if identity d\'identity')
+    identite_type = fields.Selection([('id', 'Create national d\'identite'),('carte_visit', 'create the visit'),('passport', 'Passport')],string='Type of the d\'identite')
+    identite_no = fields.Char(string='Nomber of d\'identite')
     city = fields.Char()
-    type = fields.Selection([('contact','Contact')], string='Address Type', help="Used to select automatically the right address according to the context in sales and purchases documents.")
+    type = fields.Selection([('contact', 'Contact')], string='Address Type', help="Used to select automatically the right address according to the context in sales and purchases documents.")
     city = fields.Char()
-    contact_registeration = fields.One2many('lb.contact','contact_id', string="Contact")
+    registering_contact = fields.One2many('lb.contact', 'contact_id', string="Contact")
 
 class Contact(models.Model):
     _name = 'lb.contact'
 
     contact_id = fields.Many2one('res.partner', ondelete='cascade', string="Contact")
-    contact_name = fields.Char(string="Contact Name", required=True)
+    contact_name = fields.Char(string="Name of Contact", required=True)
     contact_phone = fields.Char(string="Telephone", required=True)
     contact_email = fields.Char(string="E-mail")
     contact_notes = fields.Text(string="Notes")
