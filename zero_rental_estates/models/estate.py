79dabb0ffb68f16525f183e1e5cb34ecaa8a3f34
ZeroSystems <sales@erpzero.com>
2019-09-05 00:55:25 +0200

    Update estate.py

--- zero_rental_estates/models/estate.py
+++ zero_rental_estates/models/estate.py
@@ -18,17 +18,17 @@ class Estate(models.Model):
         country = self.env['res.country'].search([('code', '=', 'EG')], limit=1)
         return country
 
-    lessor_id = fields.Many2one('lb.lessor', ondelete='cascade', string="lessor", store=True)
+    lessor_id = fields.Many2one('lb.lessor', ondelete='cascade', string="Lessor", store=True)
     type_id = fields.Many2one('lb.type', ondelete='cascade', string="Type")
-    reference = fields.Char(string="estate Reference")
-    rental_price = fields.Float(string="rental price without charges", default=0.0, digits=dp.get_precision('Rental price'))
+    reference = fields.Char(string="Reference of Estate")
+    rental_price = fields.Float(string="price of the rental (without charges)", default=0.0, digits=dp.get_precision('price of the rental'))
     street = fields.Char()
     description = fields.Text(string="Description")
     area = fields.Integer(string="Area")
-    postal_code = fields.Char(string="Postal Code")
+    postale_code = fields.Char(string="Postal code")
     city = fields.Many2one('lb.city')
     district = fields.Many2one('lb.district')
-    country = fields.Many2one('res.country', string="Country", default=_get_default_country)
+    country = fields.Many2one('res.country', string="Countries", default=_get_default_country)
     notes = fields.Text(string="Notes")
     doc_count = fields.Integer(compute='_compute_attached_docs_count', string="Documents")
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env['res.company']._company_default_get('lb.rental'), index=1)
@@ -37,11 +37,11 @@ class Estate(models.Model):
     _sql_constraints = [     
     ('reference_unique',
     'UNIQUE(reference)',
-    "Reference must be Unique"),
+    "the reference of estate must be unique"),
     ]
 
 
-            # 2 fonctions for image attache
+            # 2 functions for the attached image
 
     def _compute_attached_docs_count(self):
         Attachment = self.env['ir.attachment']
@@ -61,7 +61,7 @@ class Estate(models.Model):
             'view_mode': 'kanban,tree,form',
             'view_type': 'form',
             'help': _('''<p class="oe_view_nocontent_create">
-                        click to add image to your estate .</p><p>
+                        Click create (not import) to add the images.</p><p>
                     </p>'''),
             'limit': 80,
             'context': "{'default_res_model': '%s','default_res_id': %d}" % (self._name, self.id)
